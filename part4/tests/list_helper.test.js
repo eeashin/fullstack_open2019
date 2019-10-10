30535e8bb94a3ca9e456773ae3a725058f9fcf03
Eashin Matubber <eashin@gmail.com>
2019-10-11 01:50:28 +0300

    4.6*: helper functions and unit tests, step4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,4 +1,5 @@
 const listHelper = require('../utils/list_helper')
+const db_blogs = require('./db.blogs')
 
 test('dummy returns one', () => {
   const blogs = []
@@ -6,3 +7,43 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+    
+      test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+      })
+    test('total likes', () => {
+      const result = listHelper.totalLikes(db_blogs)
+      expect(result).toBe(36)
+    })
+    test('favoriteBlog', () => {
+      const result = listHelper.favoriteBlog(db_blogs)
+        expect(result).toEqual({
+            _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+        })
+    })
+    test('mostBlog', () => {
+        const result = listHelper.mostBlogs(db_blogs)
+        expect(result).toEqual({
+            author: 'Robert C. Martin',
+            blogs:3
+        })
+    })
+  })
