75a98490729873dd60b5da0f26a64daee4f1f7cf
Eashin Matubber <eashin@gmail.com>
2019-12-28 04:33:46 +0200

    4.19, 4.20*

--- part4/app.js
+++ part4/app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
-const middleware = require("./utils/middleware");
-const mongoose = require("mongoose");
-console.log("connecting to", config.MONGODB_URI);
 const blogsRouter = require("./controllers/blog");
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware = require("./utils/middleware");
+const mongoose = require("mongoose");
 const logger = require("./utils/logger");
+
 logger.info("connecting to", config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI, { useNewUrlParser: true })
@@ -23,11 +23,11 @@ mongoose
 app.use(cors());
 app.use(express.static("build"));
 app.use(bodyParser.json());
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
