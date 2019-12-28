c35e10bd2b2e2bc9d3a56b31fd5706280ca6cda6
Eashin Matubber <eashin@gmail.com>
2019-12-28 23:47:08 +0200

    5.4*: bloglist frontend, step4

--- part5/src/components/Notify.js
+++ part5/src/components/Notify.js
@@ -3,13 +3,20 @@ import React from 'react'
 const Notify = ({ message }) => {
     if (message === null || message === '') {
         return null
+    } else if (message === 'Invalid username or password') {
+        return (
+            <div className="error">
+                {message}
+            </div>
+        )
+    } else {
+        return(
+            <div className="success">
+                {message}
+            </div>
+        )
     }
-
-    return (
-        <div className="success">
-            {message}
-        </div>
-    )
+    
 };
 
 export default Notify
