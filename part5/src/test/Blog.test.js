95e50e96d102e1a502066247221489995681d753
Eashin Matubber <eashin@gmail.com>
2019-12-30 00:35:50 +0200

    5.15*: Blog list tests, step3

--- part5/src/test/Blog.test.js
+++ part5/src/test/Blog.test.js
@@ -1,32 +1,52 @@
 import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
-import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/react'
 
-const testBlog = [{
 
-    title: "Test Title",
-    author: "Test Writer",
-    url: "https://www.test.fi",
-    likes: 99,
+afterEach(cleanup)
+
+describe('<Blog />', () => {
+  let component
+
+  const blog = {
+    title: 'its a blog testing!',
+    author: 'eash',
+    url: 'eash.fi',
+    likes: 1,
     user: {
-        username: "user"
+      name: 'eash'
     }
-}];
+  }
 
+  beforeEach(() => {
 
-const mocFunction = jest.fn()
+    component = render(
+      <Blog blog={blog} />
+    )
+  })
 
-test("name and author of the blog post shown",() => {
-    testBlog.map( blog =>{
-        const component = render(
-            <Blog blogs={testBlog} user={thisUserName} deleteList={mocFunction} />  
-        );
+  test(' Blog component invisible toggleable', () => {
+    const div =  component.container.querySelectorAll('div')
+    expect(div[2]).toHaveStyle('display: none')
+  })
 
-        expect(component.container).toHaveTextContent(`${m.title}${m.author}`);
-        expect(component.getByTestId("visible")).toBeVisible(); 
+  test('title and author shown', () => {
+    const div =  component.container.querySelectorAll('div')
 
+    expect(div[0]).toHaveStyle('display: block')
+    expect(div[1]).toHaveTextContent('its a blog testing!')
+  })
 
-    })
+  test('on click visible', () => {
+    const fire = component.container.querySelector('div')
+    fireEvent.click(fire)
+    const div =  component.container.querySelectorAll('div')
 
-});
+    expect(div[2]).toHaveStyle('display: none')
+    expect(div[1]).toHaveStyle('display: block')
+    expect(div[1]).toHaveTextContent('its a blog testing!')
+
+  })
+
+ 
+})
