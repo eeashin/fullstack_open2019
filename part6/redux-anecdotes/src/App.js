c38628164e2fa976db5b9202d3c9d65e261109d7
Eashin Matubber <eashin@gmail.com>
2019-12-30 08:48:48 +0200

    6.5*: anecdotes, step3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,19 +5,14 @@ import CreateForm from './components/CreateForm'
 const App = (props) => {
   const anecdotes = props.store.getState()
 
-
   return (
     <div>
       <h2>Anecdotes</h2>
       {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() =>
-              props.store.dispatch(vote(anecdote.id))}>vote</button>
+          <div>{anecdote.content}</div>
+          <div> has {anecdote.votes}
+            <button onClick={() => props.store.dispatch(vote(anecdote.id))}>vote</button>
           </div>
         </div>
       )}
--- part6/redux-anecdotes/src/components/CreateForm.js
+++ part6/redux-anecdotes/src/components/CreateForm.js
@@ -4,8 +4,10 @@ import { addNew } from '../reducers/anecdoteReducer'
 const CreateForm = (props) => (
     <div>
         <h2>create new</h2>
-        <form onSubmit={(event) => {event.preventDefault();
-            props.store.dispatch(addNew(event.target.newData.value)) }}>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            props.store.dispatch(addNew(event.target.newData.value))
+        }}>
             <div><input name="newData" /></div>
             <button>create</button>
         </form>
